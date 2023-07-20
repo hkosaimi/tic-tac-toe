2d5a1ae516787b293bd6e0528718f5a95381b2a5
Hussain <hkosaimi@gmail.com>
2023-07-21 02:55:16 +0300

    hi

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ function App() {
   return (
     <>
       <h1>this is the second push</h1>
-    <p> trying to update github pages link</p>
+      <p> trying to update github pages link</p>
+      <h6>hi</h6>
     </>
   );
 }
