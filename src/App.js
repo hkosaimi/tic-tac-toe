99ec9e0c87eb8dcca618817617f6ea73afd442d7
Hussain <hkosaimi@gmail.com>
2023-07-21 04:38:16 +0300

    test again

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>this is the second push</h1>
-      <p> trying to update github pages link</p>
-      <h6>hi</h6>
+      <h1>lorem apsum whatever</h1>
     </>
   );
 }
